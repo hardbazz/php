bac0391c9c4ac784b91bf1daf1c33e3a88b0b75a
Feddman <feddevangils@gmail.com>
2015-09-09 13:18:20 +0200

    oefeningen toegevoegd

---
+++ ex_1_student.php
@@ -0,0 +1,145 @@
+<?php
+
+/********************************
+1. opdracht:
+$b = 15
+
+Schrijf een script die $b 5 keer 
+vermenigvuldigd met 3.
+
+*/
+
+
+
+
+// echo $i;
+
+echo '<br>';
+/********************************
+2. opdracht: 
+$namen = array();
+
+Stop 5 namen van klasgenoten in de $namen array.
+geef de hoeveelheid letters van het tweede item.
+
+*/
+
+
+
+echo '<br>';
+/*********************************
+3. opdracht:
+
+Maak een functie met twee parameters.
+de functie converteert een gegeven bedrag
+naar Yen of naar Dollar.
+de parameters van de functie zijn
+1. het bedrag (int)
+2. waar het naar toe moet converten.
+
+Zo moet het aangeroepen worden:
+echo convert(200, 'Yen');
+
+*/
+
+
+
+echo '<br>';
+/***********************************
+4. opdracht:
+
+$array = [23, 55, 89, 1, 24, 66];
+
+a. Vind het hoogste getal uit de array;
+b. vind het verschil tussen eerste en laatste item;
+c. Geef het totaal van alle items;
+
+*/
+
+
+echo '<br>';
+
+/***********************************
+6. opdracht:
+
+maak een script dat het volgende laat zien op één lijn:
+1-2-3-4-5-6-7-8-9-10
+
+
+*/
+
+
+echo '<br>';
+/*************************************
+7. opdracht:
+
+maak een script die checkt of het woordje 'php' in deze zin staat:
+"Staat het woord php in deze zin?";
+
+
+*/
+
+
+echo '<br>';
+/***************************************
+
+8. opdracht:
+
+maak een script die de username van een email pakt.
+(f.vangils@rocwb.nl wordt f.vangils)
+
+
+
+*/
+
+echo '<br>';
+
+/****************************************
+9. opdracht:
+
+schrijf een php script die de laatste karakters na een url pakt.
+bijv. http://www.barrocit.nl/users/2234 wordt 2234.
+
+
+*/
+
+
+
+echo '<br>';
+/***************************************
+10. opdracht:
+
+schrijf een php script die een nieuwe item ergens in een array plaatst.
+voorbeeld:
+originele array: 1, 2, 3, 4, 5, 6, 7
+
+array met nieuw item: 1, 2, 3, 4, $, 5, 6, 7
+
+
+
+*/
+
+
+echo '<br>';
+
+/*
+11. opdracht:
+
+schrijf een php script die alle componenten van de url http://www.barrocit.nl/users/2234 in een associatieve array zet
+output:
+'schema'	 => 'http',
+'host'		 => 'www.barrocit.nl',
+'Path'		 => '/users/2234'
+
+
+*/
+
+
+
+echo '<br>';
+
+
+
+
+
+?>
