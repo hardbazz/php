b4b6af5a352803647f0886b763c82c9ad0dd9dcc
Feddman <feddevangils@gmail.com>
2015-09-22 16:42:42 +0200

    commit after lesson 2

--- projectstructuur_start/public/index.php
+++ projectstructuur_start/public/index.php
@@ -1,3 +1,24 @@
-<?php require __DIR__ . '/header.php'; ?>
+<?php require_once __DIR__ . '/header.php';?>
 
-<?= APP_FOLDER ?>
+<?php
+// de query die gedaan moet worden
+$sql = "SELECT * FROM contacts";
+// de query wordt opgeslagen
+$query = $db->query($sql);
+// de data wordt opgeslagen
+$contacts = $query->fetchAll();
+?>
+
+<div class="container">
+    <h1>Contacts</h1>
+    <ul class="list-group">
+        <?php foreach( $contacts as $contact ): ?>
+            <li class="list-group-item">
+                <?= $contact['username'] ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+</div>
+
+<?php require_once __DIR__ . '/footer.php'; ?>
